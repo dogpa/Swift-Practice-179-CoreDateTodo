2d2713e61a06989b689a8021d62b5d1e7a155a9e
Dogpa <dogpa0130@gmail.com>
2022-10-27 20:32:22 +0800

    Initial Commit

---
+++ Swift Practice 179 CoreDateTodo/Swift_Practice_179_CoreDateTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_179_CoreDateTodoApp.swift
+//  Swift Practice 179 CoreDateTodo
+//
+//  Created by Dogpa's MBAir M1 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_179_CoreDateTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
